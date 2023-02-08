5dc2727ac48f519aedd3c64cc321c153a618f903
Jeremy <jeremysiviholya@outlook.fr>
2023-02-08 14:19:34 +0100

    learn css and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-const wrapper=document.querySelector('.wrapper')
+const wrapper=document.querySelector('.wrapper'),
+musiImg=wrapper.querySelector('img'),
+musicName=wrapper.querySelector('.name'),
+musicArtist=w.querySelector('.artist'),
+playpauseBtn=wrapper.querySelector('.play-pause'),
+prevBtn=wrapper.querySelector('#prev'),
+nextBtn=wrapper.querySelector('#next'),
+mainAudi=wrapper.querySelector('#main-audio'),
+progressArea=wrapper.querySelector('.progress-area'),
+progressBar=progressArea.querySelector('progress-bar');
+
+let musicIndex=Math.floor((Math.random() * allMusic.lenght)+1)
